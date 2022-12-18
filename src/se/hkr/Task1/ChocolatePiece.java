99ef61c6b0c9126998e4c0457a31d59ae7a0bbb5
Kevin Wilusz <kwilusz10@gmail.com>
2022-12-18 22:36:43 +0100

    done with task 1

--- src/se/hkr/Task1/ChocolatePiece.java
+++ src/se/hkr/Task1/ChocolatePiece.java
@@ -2,38 +2,36 @@ package se.hkr.Task1;
 
 import java.lang.reflect.Array;
 
-public class ChocolatePiece{
-     String shape;
-     int weight;
-     boolean eaten;
+public class ChocolatePiece {
+    String shape;
+    int weight;
+    boolean eaten = false;
 
 
     public String getShape() {
         return shape;
     }
+
     public int getWeight() {
         return weight;
-
     }
+
     public boolean getEaten() {
         return eaten;
     }
 
-    public ChocolatePiece(String shape, int weight){
+    public ChocolatePiece(String shape, int weight) {
         this.shape = shape;
         this.weight = weight;
     }
+
     public void setEaten(boolean eaten) {
-        this.eaten = false;
+        this.eaten = true;
+    }
+    public boolean IsEaten() {
+        return eaten;
+        }
     }
 
 
-    public boolean BeenEaten() {
-        if(!eaten){
-            return eaten = true;
-        }
-        else {
-                return true;
-        }
-    }
-}
+
--- src/se/hkr/Task1/Main.java
+++ src/se/hkr/Task1/Main.java
@@ -1,57 +1,65 @@
 package se.hkr.Task1;
+
 import java.lang.reflect.Array;
+import java.util.Objects;
 import java.util.Random;
 
 public class Main {
 
     ChocolatePiece[] storeChocolateObject() {
-        ChocolatePiece[] chocolatePieces = new ChocolatePiece[24];
         Random random = new Random();
+        ChocolatePiece[] chocolatePieces = new ChocolatePiece[24];
         for (int i = 0; i < chocolatePieces.length; i++) {
-            int rndnr = random.nextInt(0-3);
+            int rndnr = random.nextInt(0, 3);
             if (rndnr == 0) {
                 chocolatePieces[i] = new ChocolatePiece("star", 7);
-            }else if(rndnr == 1){
+            } else if (rndnr == 1) {
                 chocolatePieces[i] = new ChocolatePiece("santa", 8);
-            }else if(rndnr == 2) {
-                chocolatePieces[i] = new ChocolatePiece("tree", 6);}
-
-
-
+            } else if (rndnr == 2) {
+                chocolatePieces[i] = new ChocolatePiece("tree", 6);
+            }
         }
         return chocolatePieces;
     }
 
-    void openCalendar() {
-        Boolean notDone = true;
-        while (notDone == true) {
-            System.out.printf("What window (day) should I open now between 1 and 24?");
-            notDone = false;
-            java.util.Scanner input = new java.util.Scanner(System.in);
-            String answer = input.nextLine();
-            try {
-                Integer.parseInt(answer);
-            } catch (Exception e) {
-                System.out.println("Try again! but without those chars dumbass!");
-                notDone = true;
-            }
-            int nr = Integer.parseInt(answer);
-            if (nr < 1 | nr > 24) {
-                notDone = true;
-                System.out.println("This isnt between 1 and 24 motherfucker!");
-            }
-        }
+    void openCalendar(ChocolatePiece[] pieces) {
+        int i = 0;
+            while (true) {
+                System.out.printf("What window (day) should I open now between 1 and 24?");
+                java.util.Scanner input = new java.util.Scanner(System.in);
+                String answer = input.nextLine();
+                try {
+                    Integer.parseInt(answer);
+                } catch (Exception e) {
+                    System.out.println("Try again! but without those chars dumbass!");
+                    continue;
+                }
+                int nr = Integer.parseInt(answer);
+                if (nr < 1 | nr > 24) {
+                    System.out.println("This isnt between 1 and 24 motherfucker!");
+                    continue;
+                }
 
-
+                ChocolatePiece piece = pieces[nr - 1];
+                if (!piece.IsEaten()){
+                    piece.setEaten(true);
+                    System.out.println("The window"+ nr +"has been open, the weight is:" + piece.getWeight() + "   and the form" +
+                            "is: " + piece.getShape());
+                    i++;
+                }
+                else {
+                    System.out.println("its already been eaten!");
+                }
+                if (i == 24){
+                    break;
+                }
+            }
     }
+
     public static void main(String[] args) {
         Main d = new Main();
-        System.out.println(d.storeChocolateObject());
-        d.openCalendar();
+        d.openCalendar(d.storeChocolateObject());
     }
-
-
-
 
 
 }
